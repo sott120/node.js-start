061ad3e01b14ba227edc7098cd6ed4975af5d845
sott120 <psy_sott120@naver.com>
2022-05-13 16:52:25 +0900

    express-ejs-layout 추가

--- project/app.js
+++ project/app.js
@@ -2,15 +2,24 @@ const express =require("express");
 const { engine } = require("express/lib/application");
 const app = express();
 const port = 3000;
+var expressLayouts = require('express-ejs-layouts');
+
 
 const homes =require('./routes/route.js');
 app.use('/', homes); //미들웨어 등록
+app.use(expressLayouts);
+
 
 app.set('views', __dirname + "/views");//html파일을 위한 경로설정
 app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+app.set('layout','layout');//폴더원하면 layout앞에 /해서 폴더생성
+app.set('layout extractScripts',true);
+
+
+// app.engine('html', require('ejs').renderFile);
 
 
 app.use(express.static(__dirname + '/public'));//css img js파일 사용을 위한 경로 설정
 //??어떻게 가능하지? html 파일 안에 링크경로 수정 없이 가능한 이유가 뭘까...
+
 module.exports = app;
--- project/routes/route.js
+++ project/routes/route.js
@@ -1,44 +1,47 @@
 const express =require("express");
+var expressLayouts = require('express-ejs-layouts');
 const router = express.Router();
 
-
+router.use(expressLayouts);
 //route, routing 파일과 경로를 지정
 router.get("/", (req,res) => {
-    res.render('index.html');
+    res.render('index');
     //서문시장야시장 메인페이지 지정
     });
     
     
     router.get("/intro", (req,res) => { //html 문서 a태그 링크 이름과 일치하게
-        res.render("intro.html");  //html 파일명과 일치하게
+        res.render("intro");  //html 파일명과 일치하게
     })
     
     router.get("/main", (req,res) => {
-        res.render("index.html"); 
+        res.render("index"); 
     })
     
     router.get("/food", (req,res) => {
-        res.render("thum.html"); 
+        res.render("thum"); 
     })
 
     router.get("/notice", (req,res) => {
-        res.render("notice.html"); 
+        res.render("notice"); 
     })
 
     router.get("/login", (req,res) => {
-        res.render("login.html"); 
+        res.render("login"); 
+        router.locals.styles = 1;
     })
 
     router.get("/view", (req,res) => {
-        res.render("notice_view.html"); 
+        res.render("notice_view"); 
     })
 
     router.get("/write", (req,res) => {
-        res.render("notice_write.html"); 
+        res.render("notice_write"); 
     })
 
     router.get("/join", (req,res) => {
-        res.render("join.html");
+        res.render("join");
+        router.locals.styles = 1;
     })
 
     module.exports = router;
